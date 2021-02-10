2b58096bd88756ea5fe17781f20651f59bba5acf
mirkovitale <mirko.vitale@giglio.com>
2021-02-10 12:55:15 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminModule } from './@admin/pages/admin.module';
+import { PublicComponent } from './@public/pages/public.component';
+import { PublicModule } from './@public/pages/public.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicComponent,
+    
   ],
   imports: [
     BrowserModule,
+    AdminModule,
+    PublicModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
